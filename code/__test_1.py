a0873481a49aabe8b5109f037e0b153420771077
Samrat <samrat.reddy@gmail.com>
2017-12-02 14:02:12 +0530

    new update

--- code/__test_1.py
+++ code/__test_1.py
@@ -12,5 +12,13 @@ df = nsfg.ReadFemPreg()
 df.describe()
 df.columns
 
+#column access
 pregorder = df['pregordr']
 pregorder = df.pregordr
+#indexing
+pregorder[0]
+#slicing
+pregorder[0:10]
+
+df['outcome'].value_counts().sort_index()
+df.birthwgt_lb.value_counts(sort=False)
